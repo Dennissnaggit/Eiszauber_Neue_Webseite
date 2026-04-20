0e213aeb5123df5a86303e71c6e68f571a139778
Dennis Falkner <dennis.falkner@snaggit.app>
2026-04-20 13:54:24 +0200

    menü gebaut und Eiskarte hinzugefügt

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+  window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".custom-navbar");
+
+    if (window.scrollY > 40) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
